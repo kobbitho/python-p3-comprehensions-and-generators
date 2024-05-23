8d0770be270b36607cb90edfc4e9010b37f32e11
Tabithaokoth <kobbitho@gmail.com>
2024-05-24 00:07:16 +0300

    adds complete lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    return [x for x in num_list if x % 2 == 0]
+
+num_list = [0, 1, 3, 5, 7, 8, 9]
+even_numbers = return_evens(num_list)
+print(even_numbers)
+
+
 
 def make_exclamation(sentence_list):
-    pass
+    if not sentence_list:
+        return []
+    return [sentence + '!' for sentence in sentence_list]
+
+sentence_list = ["Hello", "I'm doing great", "Python is fun"]
+exclamation_list = make_exclamation(sentence_list)
+print(exclamation_list)
+
+    
